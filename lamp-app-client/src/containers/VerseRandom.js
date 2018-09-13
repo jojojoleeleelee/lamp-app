e5949c2158c1f4fa84a680122496fa3fe2bc1cab
Joanne Lee <vernapii@gmail.com>
2018-09-12 21:05:16 -0700

    smaller functions for verses generated randomly

--- lamp-app-client/src/containers/VerseRandom.js
+++ lamp-app-client/src/containers/VerseRandom.js
@@ -5,6 +5,11 @@ import { bindActionCreators } from 'redux';
 import { Verse } from '../components/Verse'
 
 export default class VerseRandom extends React.Component {
+  handleOnClick = () => {
+    // need to create a function to load random verse
+    // this.props.loadRandomVerse()
+  }
+
   render() {
     return (
       <div className='center-align'>
