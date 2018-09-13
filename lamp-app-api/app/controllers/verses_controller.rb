49e76863995273d380081af0c99ca8f7ee0fc42d
Joanne Lee <vernapii@gmail.com>
2018-09-13 03:11:00 -0700

    api module and controller setup

--- lamp-app-api/app/controllers/verses_controller.rb
+++ lamp-app-api/app/controllers/verses_controller.rb
@@ -1,51 +1,58 @@
-class VersesController < ApplicationController
-  before_action :set_verse, only: [:show, :update, :destroy]
+module Api::V1
+  class VersesController < ApplicationController
+    before_action :set_verse, only: [:show, :update, :destroy]
 
-  # GET /verses
-  def index
-    @verses = Verse.all
+    # GET /verses
+    def index
+      @verses = Verse.all
 
-    render json: @verses
-  end
-
-  # GET /verses/1
-  def show
-    render json: @verse
-  end
-
-  # POST /verses
-  def create
-    @verse = Verse.new(verse_params)
-
-    if @verse.save
-      render json: @verse, status: :created, location: @verse
-    else
-      render json: @verse.errors, status: :unprocessable_entity
+      render json: @verses
     end
-  end
 
-  # PATCH/PUT /verses/1
-  def update
-    if @verse.update(verse_params)
+    # GET /verses/1
+    def show
       render json: @verse
-    else
-      render json: @verse.errors, status: :unprocessable_entity
     end
+
+    # POST /verses
+    def create
+      @verse = Verse.new(verse_params)
+
+      if @verse.save
+        render json: @verse, status: :created
+      else
+        render json: @verse.errors, status: :unprocessable_entity
+      end
+    end
+
+    # PATCH/PUT /verses/1
+    def update
+      if @verse.update(verse_params)
+        render json: @verse
+      else
+        render json: @verse.errors, status: :unprocessable_entity
+      end
+    end
+
+    # DELETE /verses/1
+    def destroy
+      @verse.destroy
+      if @verse.destroy
+        head :no_content, status: :ok
+      else
+        render json: @verse.errors, status: :unprocessable_entity
+      end
+    end
+
+    private
+      # Use callbacks to share common setup or constraints between actions.
+      def set_verse
+        @verse = Verse.find(params[:id])
+      end
+
+      # Only allow a trusted parameter "white list" through.
+      def verse_params
+        params.require(:verse).permit(:verse, :book, :memorized)
+      end
   end
-
-  # DELETE /verses/1
-  def destroy
-    @verse.destroy
-  end
-
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_verse
-      @verse = Verse.find(params[:id])
-    end
-
-    # Only allow a trusted parameter "white list" through.
-    def verse_params
-      params.require(:verse).permit(:verse, :book, :memorized)
-    end
 end
