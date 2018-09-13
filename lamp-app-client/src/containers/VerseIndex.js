8ed3d59ebb7b079e7938b28d7f7e911db16336fd
Joanne Lee <vernapii@gmail.com>
2018-09-13 03:12:29 -0700

    add axios

--- lamp-app-client/src/containers/VerseIndex.js
+++ lamp-app-client/src/containers/VerseIndex.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
+import axios from 'axios';
 
 import VerseCard from '../components/VerseCard'
 
