b618c025d7189dbe5706c075f336619e2e0167e7
Joanne Lee <vernapii@gmail.com>
2018-09-14 02:58:10 -0700

    experimenting

---
+++ client/src/components/VerseContainer.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class VerseContainer extends Component {
+  render() {
+    return (
+      <div>
+      Verse
+      </div>
+    )
+  }
+}
+export default VerseContainer
